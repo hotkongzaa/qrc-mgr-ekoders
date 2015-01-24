83675b1ecab6c41cb2dcec22212c6b134bf5e9d6
hotkong.1993@gmail.com <hotkong.1993@gmail.com@48cf7733-03d6-7cc4-b453-0ea2bc8484dc>
2015-01-24 04:56:19 +0000

    Changing setting page usage same with configuration file 'configuration.ini'

--- qrc-mgr-ekoders/qrc-mgr-setting-page/type_of_service_page/conn.php
+++ qrc-mgr-ekoders/qrc-mgr-setting-page/type_of_service_page/conn.php
@@ -1,10 +1,9 @@
 <?php
 
-$conn = @mysql_connect('localhost','root','root');
+$conf = parse_ini_file("../../model-db-connection/configuration.ini");
+$conn = @mysql_connect($conf['dataBaseDomain'], $conf['dataBaseUsername'], $conf['dataBasePassword']);
 if (!$conn) {
 	die('Could not connect: ' . mysql_error());
 }
 mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $conn);
-mysql_select_db('osbuilding_db', $conn);
-
-?>
+mysql_select_db($conf['dataBaseName'], $conn);
